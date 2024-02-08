eb5a18492ad044c04b4d61a53ca096527b88f94c
bkreiser <bkreiser@duck.com>
2024-02-08 18:29:38 -0500

    refactoring and documentation

--- src/main/java/com/github/SE4AIResearch/DataLeakage_Fall2023/docker_api/ConnectClient.java
+++ src/main/java/com/github/SE4AIResearch/DataLeakage_Fall2023/docker_api/ConnectClient.java
@@ -1,6 +1,7 @@
 package com.github.SE4AIResearch.DataLeakage_Fall2023.docker_api;
 
-import com.github.dockerjava.api.DockerClient;
+// Import DockerJavaAPI library
+import com.github.dockerjava.api.*;
 import com.github.dockerjava.api.command.CreateContainerResponse;
 import com.github.dockerjava.api.command.InspectContainerResponse;
 import com.github.dockerjava.api.command.PullImageResultCallback;
@@ -13,6 +14,7 @@ import com.github.dockerjava.core.DockerClientBuilder;
 import com.github.dockerjava.httpclient5.ApacheDockerHttpClient;
 import com.github.dockerjava.transport.DockerHttpClient;
 
+// Import java libraries
 import java.util.List;
 import java.io.File;
 import java.time.Duration;
@@ -20,13 +22,21 @@ import java.util.concurrent.TimeUnit;
 
 
 /**
- * This class is mean to establish a connection between the docker container and the plugin
+ * This class creates a Docker client to connect to the hosts docker daemon
  */
 public class ConnectClient {
+
     private static DockerClient dockerClient;
 
+    /**
+     * Constructor for ConnectClient object
+     * https://github.com/docker-java/docker-java/blob/main/docs/getting_started.md
+     */
     public ConnectClient() {
+        // Create a default client config
         DefaultDockerClientConfig config = DefaultDockerClientConfig.createDefaultConfigBuilder().build();
+
+        // Create a default http client
         DockerHttpClient httpClient = new ApacheDockerHttpClient.Builder()
                 .dockerHost(config.getDockerHost())
                 .sslConfig(config.getSSLConfig())
@@ -34,26 +44,10 @@ public class ConnectClient {
                 .connectionTimeout(Duration.ofSeconds(30))
                 .responseTimeout(Duration.ofSeconds(45))
                 .build();
+
+        // Create the docker client builder
         dockerClient = DockerClientBuilder.getInstance()
                 .withDockerHttpClient(httpClient).build();
-    }
-
-    public String listContainers() {
-        List<Container> containers = dockerClient.listContainersCmd().exec();
-        StringBuilder containersString = new StringBuilder();
-        for (Container c : containers) {
-            containersString.append(c.toString());
-        }
-        return containersString.toString();
-    }
-
-    public String listImages() {
-        List<Image> images = dockerClient.listImagesCmd().exec();
-        StringBuilder imagesSB = new StringBuilder();
-        for(Image i : images) {
-            imagesSB.append(i.toString());
-        }
-        return imagesSB.toString();
     }
 
     /**
@@ -69,6 +63,10 @@ public class ConnectClient {
         return false;
     }
 
+    /**
+     * Pulls the bkreiser/leakage-analysis image from dockerhub
+     * @return true if pullled, false if not
+     */
     public boolean pullImage() throws InterruptedException {
         List<SearchItem> items = dockerClient.searchImagesCmd("bkreiser01/leakage-analysis").exec();
         return dockerClient.pullImageCmd("bkreiser01/leakage-analysis")
@@ -77,20 +75,12 @@ public class ConnectClient {
                     .awaitCompletion(180, TimeUnit.SECONDS);
     }
 
-    public boolean runLeakage() {
-        return false;
-    }
-
-    public boolean createVolume(String projectPath) {
-        //Should probably return the volume path?
-        return false;
-    }
-
-    /*
-    * This function creates and run the LAT docker container
-    * @param filePath - The path to the file to run the LAT on
-    * @param fileName - The name of the file to run the LAT on
-    * @return String containing the container ID*/
+    /**
+     * This function creates and run the LAT docker container
+     * @param filePath - The path to the file to run the LAT on
+     * @param fileName - The name of the file to run the LAT on
+     * @return String containing the container ID
+     */
     public String runLeakageAnalysis(File filePath, String fileName) {
         // Get the path to the file on the users machine
         String path2file = filePath.toString();
@@ -98,8 +88,8 @@ public class ConnectClient {
         // Create the container
         CreateContainerResponse createContainerResponse = dockerClient.createContainerCmd("leakage")
                 .withImage("bkreiser01/leakage-analysis")
-                .withBinds(Bind.parse(path2file+":/execute"))
-                .withCmd("/execute/"+fileName).exec();
+                .withBinds(Bind.parse(path2file + ":/execute"))
+                .withCmd("/execute/" + fileName).exec();
 
         // Get the container's id
         String containerId = createContainerResponse.getId();
@@ -107,21 +97,7 @@ public class ConnectClient {
         // Execute the container by ID
         dockerClient.startContainerCmd(containerId).exec();
 
+        // Return the ID of the newly created container
         return containerId;
     }
-
-    public boolean x () throws InterruptedException {
-        if(!this.checkImageOnMachine()){
-            this.pullImage();
-        }
-        return true;
-    }
-
-    /**
-     * Function that uses the docker image to run things
-     */
-    public void performAnalysis() {
-        // Want it to return the root location of the output probably
-    }
-
 }
