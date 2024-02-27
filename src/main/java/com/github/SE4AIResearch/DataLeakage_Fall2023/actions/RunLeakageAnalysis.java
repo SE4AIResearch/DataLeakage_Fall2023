2f0ef750083d2a348dd018bf76bf6a3d59516867
Roger Shagawat <34222430+rogerShagawat@users.noreply.github.com>
2024-02-27 15:51:35 -0500

    updated ui

--- src/main/java/com/github/SE4AIResearch/DataLeakage_Fall2023/actions/RunLeakageAnalysis.java
+++ src/main/java/com/github/SE4AIResearch/DataLeakage_Fall2023/actions/RunLeakageAnalysis.java
@@ -29,8 +29,8 @@ import static com.intellij.openapi.actionSystem.IdeActions.ACTION_INSPECT_CODE;
 
 public class RunLeakageAnalysis extends AnAction {
 
-   private ConnectClient connectClient = new ConnectClient();
-   private FileChanger fileChanger = new FileChanger();
+   private final ConnectClient connectClient = new ConnectClient();
+   private final FileChanger fileChanger = new FileChanger();
 
    private static Project getProjectForFile(VirtualFile file) {
       Project project = null;
