cafe885ff17adc699d9e5cf202eb1090e112acc8
Roger Shagawat <34222430+rogerShagawat@users.noreply.github.com>
2024-02-28 17:53:19 -0500

    prevent ui changes outside edt thread

--- src/main/java/com/github/SE4AIResearch/DataLeakage_Fall2023/actions/RunLeakageAnalysis.java
+++ src/main/java/com/github/SE4AIResearch/DataLeakage_Fall2023/actions/RunLeakageAnalysis.java
@@ -36,6 +36,8 @@ public class RunLeakageAnalysis extends AnAction {
 
    private boolean isCompleted = false;
 
+   private boolean isCompleted = false;
+
    private static Project getProjectForFile(VirtualFile file) {
       Project project = null;
       if (file != null) {
