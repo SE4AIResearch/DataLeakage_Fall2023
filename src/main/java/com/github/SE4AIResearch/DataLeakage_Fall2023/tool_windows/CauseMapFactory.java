1ff61a289e9b44a654097046ff0ad00c6115a8b0
Roger Shagawat <34222430+rogerShagawat@users.noreply.github.com>
2024-03-13 11:24:36 -0400

    added causemapfactory

--- src/main/java/com/github/SE4AIResearch/DataLeakage_Fall2023/tool_windows/CauseMapFactory.java
+++ src/main/java/com/github/SE4AIResearch/DataLeakage_Fall2023/tool_windows/CauseMapFactory.java
@@ -1,5 +1,19 @@
 package com.github.SE4AIResearch.DataLeakage_Fall2023.tool_windows;
 
-public record CauseText() {
-   
+import com.github.SE4AIResearch.DataLeakage_Fall2023.enums.LeakageCause;
+
+import java.util.HashMap;
+
+public class CauseMapFactory {
+   public static HashMap<LeakageCause, String> getCauseMap() {
+      HashMap<LeakageCause, String> map = new HashMap<>();
+
+      map.put(LeakageCause.SplitBeforeSample, "Data split before sampling.");
+      map.put(LeakageCause.DataAugmentation, "Use caution when performing data augmentation.");
+      map.put(LeakageCause.VectorizingTextData, "Vectorizer fit on train and test data together.");
+      map.put(LeakageCause.RepeatDataEvaluation, "Same data used for multiple evalutations");
+      map.put(LeakageCause.unknown, "Unknown");
+
+      return map;
+   }
 }
