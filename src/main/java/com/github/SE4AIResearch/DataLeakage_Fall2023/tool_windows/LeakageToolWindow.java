88e72fac33412fd3fef13c33702c00a29b13cf3f
cd721 <cdemario@stevens.edu>
2024-03-31 22:25:50 -0400

    update tool window with notifier

--- src/main/java/com/github/SE4AIResearch/DataLeakage_Fall2023/tool_windows/LeakageToolWindow.java
+++ src/main/java/com/github/SE4AIResearch/DataLeakage_Fall2023/tool_windows/LeakageToolWindow.java
@@ -5,6 +5,7 @@ import com.github.SE4AIResearch.DataLeakage_Fall2023.data.LeakageSource;
 import com.github.SE4AIResearch.DataLeakage_Fall2023.data.Utils;
 import com.github.SE4AIResearch.DataLeakage_Fall2023.enums.LeakageCause;
 import com.github.SE4AIResearch.DataLeakage_Fall2023.enums.LeakageType;
+import com.github.SE4AIResearch.DataLeakage_Fall2023.inspections.QuickFixActionNotifier;
 import com.github.SE4AIResearch.DataLeakage_Fall2023.parsers.LeakageAnalysisParser;
 import com.intellij.ide.BrowserUtil;
 import com.intellij.ide.DataManager;
@@ -86,7 +87,19 @@ public class LeakageToolWindow implements ToolWindowFactory, DumbAware {
             this.runLeakageAction = new RunLeakageAction(project);
 
             toolWindow.getComponent().add(contentPanel);
+            project.getMessageBus().connect().subscribe(QuickFixActionNotifier.QUICK_FIX_ACTION_TOPIC, new QuickFixActionNotifier(){
+                @Override
+                public void beforeAction(){
 
+                }
+
+                @Override
+
+                public void afterAction(){
+                    updateTableData();
+                }
+
+            });
             updateTableData();
         }
 
