d3bff2147d74daffc66f813710d85b86cb954add
cd721 <cdemario@stevens.edu>
2024-05-13 14:56:04 -0400

    resize cols

---
+++ src/main/java/com/github/SE4AIResearch/DataLeakage_Fall2023/tool_windows/CustomTableColumnModel.java
@@ -0,0 +1,7 @@
+package com.github.SE4AIResearch.DataLeakage_Fall2023.tool_windows;
+
+import javax.swing.table.DefaultTableColumnModel;
+
+public class CustomTableColumnModel extends DefaultTableColumnModel {
+
+}
